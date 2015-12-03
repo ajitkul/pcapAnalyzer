f9257cdb859671a477f05042c175e05ec6fc41a8
Ajit <ajit.kulkarni@synerzip.com>
2015-12-03 18:20:17 +0530

    Added a route

--- src/bin/js/pcapAnalyzer.js
+++ src/bin/js/pcapAnalyzer.js
@@ -1,1 +1,11 @@
 var app = angular.module('pcapApp', ['ui.router']);
+
+app.config(function ($stateProvider,$urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider.state('home', {
+		url: '/home',
+		template: '<h1>Hi</h1>'
+	});
+});
