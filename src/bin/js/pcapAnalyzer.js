8379c870aa87846355dd13883f159615dd430a80
Ajit <ajit.kulkarni@synerzip.com>
2015-12-09 14:55:01 +0530

    Added Previous Reports Mockup

--- src/bin/js/pcapAnalyzer.js
+++ src/bin/js/pcapAnalyzer.js
@@ -161,3 +161,141 @@ app.controller('MainCtrl', ['$scope', function ($scope) {
 app.controller('FileUploadCtrl', ['$scope', function ($scope) {
 
 }]);
+
+app.controller('NavCtrl', ['$rootScope', '$scope', '$http', 'modal', '$interval', function ($rootScope, $scope, $http, modal, $interval) {
+	var myModal = new modal();
+ 
+  $scope.hideGrid = true;
+ 
+  $rootScope.gridOptions = {
+    onRegisterApi: function (gridApi) {
+      $scope.gridApi = gridApi;
+ 
+      // call resize every 200 ms for 2 s after modal finishes opening - usually only necessary on a bootstrap modal
+      $interval( function() {
+        $scope.gridApi.core.handleWindowResize();
+      }, 10, 500);
+      }
+  };
+ 
+  $rootScope.gridOptions.data = [
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	},
+	{
+		'VLANName': 'LibreOff/Prezi',
+		'fileName': 'LibreOff/Prezi_50_8122015_logs.pcap',
+		'reportDate': '8/12/2015 16:25'
+	}
+	];
+
+  $scope.showModal = function() {
+    myModal.open();
+  };
+}]);
+
+app.factory('modal', ['$compile', '$rootScope', function ($compile, $rootScope) {
+  return function() {
+    var elm;
+    var modal = {
+      open: function() {
+ 
+        var html = '<div class="modal" ng-style="{{modalStyle}}"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"></div><div class="modal-body"><div id="grid12" ui-grid="gridOptions" class="grid"></div></div><div class="modal-footer"><button id="buttonClose" class="btn btn-primary" ng-click="close()">Close</button></div></div></div></div>';
+        elm = angular.element(html);
+        angular.element(document.body).prepend(elm);
+ 
+        $rootScope.close = function() {
+          modal.close();
+        };
+        
+        $rootScope.modalStyle = {"display": "block"};
+ 
+        $compile(elm)($rootScope);
+      },
+      close: function() {
+        if (elm) {
+          elm.remove();
+        }
+      }
+    };
+ 
+    return modal;
+  };
+}]);
